02a86256469b1fe2e37a685ed07a63d929f2e7d4
Eli Case <elicase4@gmail.com>
2025-06-15 16:23:12 -0500

    edit: add clang lsp

--- Linux/nvim.init.lua
+++ Linux/nvim.init.lua
@@ -637,7 +637,9 @@ require("lazy").setup({
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
-				-- clangd = {},
+				clangd = {
+					cmd = { "clangd" },
+				},
 				-- gopls = {},
 				-- pyright = {},
 				-- rust_analyzer = {},
@@ -682,6 +684,7 @@ require("lazy").setup({
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				"stylua", -- Used to format Lua code
+				"clang-format", -- Clang lsp
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
